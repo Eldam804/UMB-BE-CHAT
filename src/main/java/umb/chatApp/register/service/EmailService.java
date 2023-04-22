8d650adb8899f7da08f16226daf2b3d032adf5ad
Eldam804 <adam.kleinedler@gmail.com>
2023-04-22 21:15:39 +0200

    secured api key

--- src/main/java/umb/chatApp/register/service/EmailService.java
+++ src/main/java/umb/chatApp/register/service/EmailService.java
@@ -5,6 +5,7 @@ import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     //EMAIL je potrebne na free uctoch pridat cez app.mailgun
-    private static final String MAILGUN_API_KEY = "e9cbb34880cd156b3482d484ea26f982-181449aa-001c802d";
+    @Value("${mailgun.api-key}")
+    private String MAILGUN_API_KEY;
     private static final String MAILGUN_DOMAIN = "postmaster@sandboxf9fc71cc544f42cf8ce4173b5bc1719d.mailgun.org";
     public void sendEmail(String to, String subject, String body){
+        System.out.println("API KEY: " +MAILGUN_API_KEY);
         Email email = EmailBuilder.startingBlank()
                 .from("ChatApp", "chatapplicationnoreply@gmail.com")
                 .to("you", to)
